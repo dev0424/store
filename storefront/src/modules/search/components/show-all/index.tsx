2daa089a0a4d825c4357280ad12628529e15bd01
Zoltan Maroti <zoli.maroti@gmail.com>
2025-05-05 19:12:34 +0200

    Style show all dropdown

--- storefront/src/modules/search/components/show-all/index.tsx
+++ storefront/src/modules/search/components/show-all/index.tsx
@@ -1,33 +1,39 @@
-import { Container, Text } from "@medusajs/ui"
-import { useHits, useSearchBox } from "react-instantsearch-hooks-web"
-
-import InteractiveLink from "@modules/common/components/interactive-link"
+import { Text } from "@medusajs/ui";
+import { useHits, useSearchBox } from "react-instantsearch-hooks-web";
+import InteractiveLink from "@modules/common/components/interactive-link";
 
 const ShowAll = () => {
-  const { hits } = useHits()
-  const { query } = useSearchBox()
-  const width = typeof window !== "undefined" ? window.innerWidth : 0
+  const { hits } = useHits();
+  const { query } = useSearchBox();
+  const width = typeof window !== "undefined" ? window.innerWidth : 0;
 
-  if (query === "") return null
-  if (hits.length > 0 && hits.length <= 6) return null
+  if (query === "") {
+    return null;
+  }
+
+  if (hits.length > 0 && hits.length <= 6) {
+    return null;
+  }
 
   if (hits.length === 0) {
     return (
-      <Container
-        className="flex gap-2 justify-center h-fit py-2"
+      <div
+        className="flex h-fit justify-center gap-2 py-2"
         data-testid="no-search-results-container"
       >
-        <Text>No results found.</Text>
-      </Container>
-    )
+        <Text className="text-ui-fg-subtle">No results found.</Text>
+      </div>
+    );
   }
 
   return (
-    <Container className="flex sm:flex-col small:flex-row gap-2 justify-center items-center h-fit py-4 small:py-2">
-      <Text>Showing the first {width > 640 ? 6 : 3} results.</Text>
+    <div className="flex h-fit items-center justify-center gap-2 py-4 sm:flex-col small:flex-row small:py-2">
+      <Text className="text-ui-fg-subtle">
+        Showing the first {width > 640 ? 6 : 3} results.
+      </Text>
       <InteractiveLink href={`/results/${query}`}>View all</InteractiveLink>
-    </Container>
-  )
-}
+    </div>
+  );
+};
 
-export default ShowAll
+export default ShowAll;
