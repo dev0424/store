c958d566d64b2557439f28ea29f5b07d5d05ba8e
Zoltan Maroti <zoli.maroti@gmail.com>
2025-05-12 13:22:35 +0200

    Add footer links

--- storefront/src/modules/layout/templates/footer/index.tsx
+++ storefront/src/modules/layout/templates/footer/index.tsx
@@ -20,7 +20,7 @@ export default async function Footer() {
               href="/"
               className="txt-compact-xlarge-plus uppercase text-ui-fg-subtle hover:text-ui-fg-base"
             >
-              Medusa Store
+              Store
             </LocalizedClientLink>
           </div>
           <div className="text-small-regular grid grid-cols-2 gap-10 sm:grid-cols-3 md:gap-x-16">
@@ -109,37 +109,39 @@ export default async function Footer() {
               </div>
             )}
             <div className="flex flex-col gap-y-2">
-              <span className="txt-ui-fg-base txt-small-plus">Medusa</span>
+              <span className="txt-ui-fg-base txt-small-plus">Links</span>
               <ul className="txt-small grid grid-cols-1 gap-y-2 text-ui-fg-subtle">
                 <li>
-                  <a
-                    href="https://github.com/medusajs"
-                    target="_blank"
-                    rel="noreferrer"
+                  <LocalizedClientLink
                     className="hover:text-ui-fg-base"
+                    href="#"
                   >
-                    GitHub
-                  </a>
+                    Help
+                  </LocalizedClientLink>
                 </li>
                 <li>
-                  <a
-                    href="https://docs.medusajs.com"
-                    target="_blank"
-                    rel="noreferrer"
+                  <LocalizedClientLink
                     className="hover:text-ui-fg-base"
+                    href="#"
                   >
-                    Documentation
-                  </a>
+                    Contact
+                  </LocalizedClientLink>
                 </li>
                 <li>
-                  <a
-                    href="https://github.com/medusajs/nextjs-starter-medusa"
-                    target="_blank"
-                    rel="noreferrer"
+                  <LocalizedClientLink
                     className="hover:text-ui-fg-base"
+                    href="#"
                   >
-                    Source code
-                  </a>
+                    Privacy policy
+                  </LocalizedClientLink>
+                </li>
+                <li>
+                  <LocalizedClientLink
+                    className="hover:text-ui-fg-base"
+                    href="#"
+                  >
+                    Cookie policy
+                  </LocalizedClientLink>
                 </li>
               </ul>
             </div>
@@ -147,9 +149,8 @@ export default async function Footer() {
         </div>
         <div className="mb-16 flex w-full justify-between text-ui-fg-muted">
           <Text className="txt-compact-small">
-            © {new Date().getFullYear()} Medusa Store. All rights reserved.
+            © {new Date().getFullYear()} Store. All rights reserved.
           </Text>
-          <MedusaCTA />
         </div>
       </div>
     </footer>
