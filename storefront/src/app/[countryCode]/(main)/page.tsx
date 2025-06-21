41c0c41ad1059751c017c2c7bf0ca7d5a391f6c6
zoltanMaroti <zoli.maroti@gmail.com>
2025-06-21 10:54:46 +0200

    Benefits banner

--- storefront/src/app/[countryCode]/(main)/page.tsx
+++ storefront/src/app/[countryCode]/(main)/page.tsx
@@ -5,6 +5,7 @@ import Hero from "@modules/home/components/hero";
 import { listCollections } from "@lib/data/collections";
 import { getRegion } from "@lib/data/regions";
 import FeaturedCategories from "@modules/home/components/featured-categories";
+import BenefitsBanner from "@modules/home/components/benefits-banner";
 
 export const metadata: Metadata = {
   title: "Medusa Next.js Starter Template",
@@ -38,6 +39,7 @@ export default async function Home(props: {
       <ul className="content-container flex flex-col gap-x-6">
         <FeaturedProducts collections={collections} region={region} />
       </ul>
+      <BenefitsBanner />
     </div>
   );
 }
