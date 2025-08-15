c2c7d95909acc2dbeddb8e6d04defda32959b28e
zoltanMaroti <zoli.maroti@gmail.com>
2025-08-15 17:51:13 +0200

    Show category image in desktop navigation menu

--- storefront/src/modules/categories/components/category-menu-content/index.tsx
+++ storefront/src/modules/categories/components/category-menu-content/index.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import { StoreProductCategory } from "@medusajs/types";
 import Link from "next/link";
 import { hasChildrenCategory } from "@lib/util/categories";
+import Thumbnail from "@modules/products/components/thumbnail";
 
 type Props = {
   category: StoreProductCategory;
@@ -10,13 +11,18 @@ type Props = {
 const CategoryMenuContent = ({ category }: Props) => {
   return (
     <div className="w-screen bg-background-primary text-ui-fg-on-color">
-      <ul className="content-container flex list-none gap-16 py-8">
+      <ul className="content-container flex list-none gap-8 py-8">
         {category.category_children.map((subCategory) => (
-          <li key={subCategory.id} className={"flex flex-col gap-2"}>
+          <li key={subCategory.id}>
             <Link
-              className={"font-black"}
+              className={"flex flex-col gap-2 font-normal"}
               href={`/categories/${subCategory.handle}`}
             >
+              <Thumbnail
+                thumbnail={subCategory.metadata?.thumbnail as string}
+                size="square"
+                className={"sm: w-40 rounded-md object-cover sm:h-40"}
+              />
               {subCategory.name}
             </Link>
             {hasChildrenCategory(subCategory) ? (
