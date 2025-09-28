67de7d058849d457e4b7e00771b3523f4ebc5f95
zoltanMaroti <zoli.maroti@gmail.com>
2025-09-28 14:22:43 +0200

    Category menu content has background

--- storefront/src/modules/categories/components/category-menu-content/index.tsx
+++ storefront/src/modules/categories/components/category-menu-content/index.tsx
@@ -3,46 +3,53 @@ import { StoreProductCategory } from "@medusajs/types";
 import Link from "next/link";
 import { hasChildrenCategory } from "@lib/util/categories";
 import Thumbnail from "@modules/products/components/thumbnail";
-
-type Props = {
-  category: StoreProductCategory;
-};
+import { PopoverBackdrop } from "@headlessui/react";
 
 const CategoryMenuContent = ({ category }: Props) => {
   return (
-    <div className="w-screen bg-background-primary text-ui-fg-on-color">
-      <ul className="content-container flex list-none gap-8 py-8">
-        {category.category_children.map((subCategory) => (
-          <li key={subCategory.id}>
-            <Link
-              className={"flex flex-col gap-2 font-normal"}
-              href={`/categories/${subCategory.handle}`}
-            >
-              <Thumbnail
-                thumbnail={subCategory.metadata?.thumbnail as string}
-                size="square"
-                className={"sm: w-40 rounded-md object-cover sm:h-40"}
-              />
-              {subCategory.name}
-            </Link>
-            {hasChildrenCategory(subCategory) ? (
-              <div className={"flex flex-col gap-2"}>
-                {subCategory.category_children.map((children) => (
-                  <Link
-                    key={children.id}
-                    href={`/categories/${children.handle}`}
-                    className={"text-sm"}
-                  >
-                    {children.name}
-                  </Link>
-                ))}
-              </div>
-            ) : null}
-          </li>
-        ))}
-      </ul>
+    <div>
+      <PopoverBackdrop
+        transition
+        className="data-closed:opacity-0 fixed inset-0 bg-black/30 transition duration-200 ease-out"
+      />
+      <div className="relative z-10 w-screen bg-background-primary text-ui-fg-on-color">
+        <ul className="content-container flex list-none gap-8 py-8">
+          {category.category_children.map((subCategory) => (
+            <li key={subCategory.id}>
+              <Link
+                className="flex flex-col gap-2 font-normal"
+                href={`/categories/${subCategory.handle}`}
+              >
+                <Thumbnail
+                  thumbnail={subCategory.metadata?.thumbnail as string}
+                  size="square"
+                  className="rounded-md object-cover sm:h-40 sm:w-40"
+                />
+                {subCategory.name}
+              </Link>
+              {hasChildrenCategory(subCategory) ? (
+                <div className="flex flex-col gap-2">
+                  {subCategory.category_children.map((children) => (
+                    <Link
+                      key={children.id}
+                      href={`/categories/${children.handle}`}
+                      className="text-sm"
+                    >
+                      {children.name}
+                    </Link>
+                  ))}
+                </div>
+              ) : null}
+            </li>
+          ))}
+        </ul>
+      </div>
     </div>
   );
+};
+
+type Props = {
+  category: StoreProductCategory;
 };
 
 export default CategoryMenuContent;
