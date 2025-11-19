5ae0862346b43e6fe9f5a3db731a874dc21a2335
Zoltan Maroti <zoli.maroti@gmail.com>
2025-11-19 15:36:12 +0100

    Debugging

--- storefront/src/app/[countryCode]/(main)/products/[handle]/page.tsx
+++ storefront/src/app/[countryCode]/(main)/products/[handle]/page.tsx
@@ -92,6 +92,8 @@ export default async function ProductPage(props: Props) {
     queryParams: { handle: params.handle },
   }).then(({ response }) => response.products[0]);
 
+  console.log(pricedProduct);
+
   if (!pricedProduct) {
     notFound();
   }
