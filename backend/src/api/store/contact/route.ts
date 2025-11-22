7266570f5ff380b6e8a09ae86aa5201aaec3d108
zoltanMaroti <zoli.maroti@gmail.com>
2025-11-22 18:57:04 +0100

    Debugging

--- backend/src/api/store/contact/route.ts
+++ backend/src/api/store/contact/route.ts
@@ -5,26 +5,26 @@ import { ContactForm } from '../../../lib/types';
 export const POST = async (request: MedusaRequest<ContactForm>, response: MedusaResponse) => {
     const { first_name, last_name, phone, email, message } = request.body;
 
-    try {
-        const { result } = await sendContactEmailWorkflow(request.scope).run({
-            input: {
-                first_name,
-                last_name,
-                phone,
-                email,
-                message,
-            },
-        });
+    // try {
+    const { result } = await sendContactEmailWorkflow(request.scope).run({
+        input: {
+            first_name,
+            last_name,
+            phone,
+            email,
+            message,
+        },
+    });
 
-        return response.json({
-            success: true,
-            notification: result.notification,
-        });
-    } catch (error) {
-        return response.status(500).json({
-            type: 'unexpected_state',
-            message: 'Failed to send contact email.',
-            code: 'api_error',
-        });
-    }
+    return response.json({
+        success: true,
+        notification: result.notification,
+    });
+    // } catch (error) {
+    //     return response.status(500).json({
+    //         type: 'unexpected_state',
+    //         message: 'Failed to send contact email.',
+    //         code: 'api_error',
+    //     });
+    // }
 };
