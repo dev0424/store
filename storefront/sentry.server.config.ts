bd15298c3e0a8e97afa69dfdf4dd75b6c7e2bb68
zoltanMaroti <zoli.maroti@gmail.com>
2026-01-03 15:06:48 +0100

    Storefront Sentry instrumentation

---
+++ storefront/sentry.server.config.ts
@@ -0,0 +1,19 @@
+// This file configures the initialization of Sentry on the server.
+// The config you add here will be used whenever the server handles a request.
+// https://docs.sentry.io/platforms/javascript/guides/nextjs/
+
+import * as Sentry from "@sentry/nextjs";
+
+Sentry.init({
+  dsn: "https://c7b07b65a5dce87dd774e84cc30bbc32@o4510646802644992.ingest.de.sentry.io/4510646803824720",
+
+  // Define how likely traces are sampled. Adjust this value in production, or use tracesSampler for greater control.
+  tracesSampleRate: 1,
+
+  // Enable logs to be sent to Sentry
+  enableLogs: true,
+
+  // Enable sending user PII (Personally Identifiable Information)
+  // https://docs.sentry.io/platforms/javascript/guides/nextjs/configuration/options/#sendDefaultPii
+  sendDefaultPii: true,
+});
